3da489bf1d5096afb91c62fadde71fae6f9f7c34
Battousaicr <jorge_tolkien@hotmail.com>
2023-06-02 14:12:47 -0600

    Add files via upload

---
+++ tarea-2.py
@@ -0,0 +1,27 @@
+'''
+x = num
+
+num = int(input())
+'''
+
+'''
+for x in range(0,11):
+  if x > 1:
+    for i in range(2, x):
+      if (x % i) == 0:
+        break
+    else:
+      print(x)
+'''
+
+
+for x in range(0,1001):
+  if x > 1:
+    for i in range(2, x):
+      if (x % i) == 0:
+        break
+    else:
+      print(x, end=" ")
+
+
+
